a006703c01caf19e1ea90b6e7891158d9419c03b
earljosephclaro <earljoseph.claro@sun-asterisk.com>
2025-05-06 20:47:57 +0800

    initialized mongodb setup

--- db/mongo.py
+++ db/mongo.py
@@ -2,12 +2,19 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+load_dotenv()  # Load environment variables from the .env file
 MONGO_URI = os.getenv("MONGODB_URI")
 
+# Connect to MongoDB
 client = MongoClient(MONGO_URI)
+
+# Access the database
 db = client["sellerscan"]
 
+# Access collections (you can add more collections as needed)
 users_col = db["users"]
 sellers_col = db["sellers"]
 asins_col = db["asins"]
+
+print(users_col.find_one())
+print("Connected to MongoDB:", db)
